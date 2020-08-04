31c6ac1d0d0f54c14131270c3069e7e15c1db6ed
Mario Andres <>
2020-08-04 14:15:35 -0300

    .

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from 'src/app/services/socket.service';
+import { ChatService } from 'src/app/services/chat.service';
+
 import { ProfService } from 'src/app/services/prof.service';
 import { ImageService } from 'src/app/services/image.service';
 import { NgForm } from '@angular/forms';
+
+import { Chat } from 'src/app/models/chat'
 
 @Component({
   selector: 'app-chat',
@@ -11,31 +15,27 @@ import { NgForm } from '@angular/forms';
 })
 export class ChatComponent implements OnInit {
 
-  mess: string;
   userId: string = sessionStorage.getItem('currentUserId');
-  partner: string = '';  
+  mess: string;
+  partner: string;  
   nick: string;
   Online: any[];
-
+  Active: any[] = [{
+    "prof": "",
+    "image": "",
+    "nick": ""
+  }]
 
   constructor(private socketService: SocketService,
               private imageService: ImageService,
               public profService: ProfService,
+              public chatService: ChatService,
   ) { }
 
   ngOnInit(): void {
-    this.listenMessage();
     this.listenUsers();
-  }
-
-  listenMessage(){
-    this.socketService.listen('whisper').subscribe((data: any) =>{
-      const line = document.createElement('p');
-      line.textContent = data.message;
-      line.style.minWidth = "400px";
-      line.style.color = "yellow";
-      document.getElementById('container').appendChild(line);
-    })
+    this.listenMessage();
+    this.partner = '';
   }
 
   listenUsers(){
@@ -62,14 +62,43 @@ export class ChatComponent implements OnInit {
             this.Online.push({"prof": data[i],"image": img, "nick": P[k].nickname});
           }
         }
-        
       }
     }
+  }
+
+  listenMessage(){
+    const I = this.imageService.images;
+    this.socketService.listen('whisper').subscribe((data: any) =>{
+      if(data.toUser === this.partner){
+        this.newLinePartner(data);
+        return;
+      }
+      for(var i in this.Online){
+        if(this.Online[i].prof === data.toUser){
+          console.log(this.Online[i]);
+          this.Active.push(this.Online[i]);
+          console.log(this.Active)
+          this.Online.splice(this.Online.indexOf(this.Online[i]) ,1);
+        }
+      }
+    });
+  }
+
+  newLinePartner(data: any){
+    const line = document.createElement('p');
+    line.textContent = data.message;
+    line.style.minWidth = "400px";
+    line.style.color = "yellow";
+    document.getElementById('container').appendChild(line);
   }
 
   sendChat(form: NgForm){
     let mess = { "message": form.value.message, "toUser": this.partner };
     this.socketService.emit("send message", mess);
+    this.newLineUser(form);
+  }
+
+  newLineUser(form: NgForm){
     const line = document.createElement('p');
     line.textContent = form.value.message;
     line.style.minWidth = "400px";
@@ -80,10 +109,23 @@ export class ChatComponent implements OnInit {
   }
 
   startChat(user: any){
-    console.log(this.Online)
-    const P = this.profService.profs
     this.partner = user.prof;
     this.nick = user.nick;
+    this.chatService.getChats().subscribe(res=>{
+      const C = this.chatService.chats = res as Chat[];
+      for(var i in C){
+        if(C[i].userId === user.prof){
+          this.chatService.selectedChat = C[i];
+          this.getHistory(C[i]);
+          return
+        }
+      }
+      this.chatService.selectedChat = new Chat;
+    })
+  }
+
+  getHistory(chat: Chat){
+
   }
 
   hideChat(){
@@ -91,4 +133,3 @@ export class ChatComponent implements OnInit {
   }
 
 }
-
--- src/app/models/chat.ts
+++ src/app/models/chat.ts
@@ -3,12 +3,18 @@ import { Mess } from './mess'
 export class Chat {
 
     _id: string; 
+    userId: string;
     partner: string;
     messages: Mess[];
+    date: string;
+    modified: string;
 
-    constructor(_id="", partner=""){
+    constructor(_id="", userId="", partner="", date="", modified=""){
         this._id = _id;
+        this.userId = userId;
         this.partner = partner;
         this.messages = [];
+        this.date = date;
+        this.modified = modified;
     }
 }
